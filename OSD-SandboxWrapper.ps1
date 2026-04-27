b005a10686873616f0d0b33fac9384092631b19c
amillerSSD <76002781+amillerSSD@users.noreply.github.com>
2026-04-27 16:25:39 -0700

    Update OSBuild version in OSD-SandboxWrapper.ps1
    25h2

--- OSD-SandboxWrapper.ps1
+++ OSD-SandboxWrapper.ps1
@@ -77,7 +77,7 @@ if ($WindowsPhase -eq 'WinPE') {
     #Process OSDCloud startup and load Azure KeyVault dependencies
     Import-Module OSD -Force
 
-Start-OSDCloud -OSLanguage en-us -OSBuild 24H2 -OSEdition Education -ZTI
+Start-OSDCloud -OSLanguage en-us -OSBuild 25H2 -OSEdition Education -ZTI
 
     
     #Stop the startup Transcript.  OSDCloud will create its own
